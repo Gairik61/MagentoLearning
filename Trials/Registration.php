fcbbd46ecc382c5e62015c0927ae2b3227650e43
Gairik61 <41142624+Gairik61@users.noreply.github.com>
2018-07-25 13:51:12 +0530

    Update and rename Registration.php to registration.php

--- Trials/Registration.php
+++
@@ -1,3 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponemtRegister::register(\Magento\Framework\Component\ComponemtRegister::MODULE,MagentoLearning_Trials,__DIR__);
-?>
---
+++ Trials/registration.php
@@ -0,0 +1,3 @@
+<?php
+\Magento\Framework\Component\ComponentRegister::register(\Magento\Framework\Component\ComponentRegister::MODULE,MagentoLearning_Trials,__DIR__);
+?>
